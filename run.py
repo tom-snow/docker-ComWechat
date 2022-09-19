7f3d8956b9e94a017eb006aaf2c276bb12c636e3
github_bot <github_bot@noreply.github.com>
2022-09-19 23:26:43 +0800

    触发构建镜像

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ class DockerWechatHook:
         self.exit_container()
 
     def run_vnc(self):
-        # 根据VNCPASS环境变量生成vncpasswd文件
+        # 根据 VNCPASS 环境变量生成 vncpasswd 文件
         os.makedirs('/root/.vnc', mode=755, exist_ok=True)
         passwd_output = subprocess.run(['/usr/bin/vncpasswd','-f'],input=os.environ['VNCPASS'].encode(),capture_output=True)
         with open('/root/.vnc/passwd', 'wb') as f:
